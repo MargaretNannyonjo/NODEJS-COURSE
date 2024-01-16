c22bf3bfccd15f016537e667910ffb0ed587ed8d
MargaretNannyonjo <nannyonjoleon@gmail.com>
2024-01-16 13:33:12 -0800

    Node modules and streams

---
+++ event-loops.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const crypto = require("crypto");
+
+const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 1;
+
+setTimeout(() => console.log("Timer 1 finished"), 0);
+setImmediate(() => console.log("Imediate 1 finished"));
+
+fs.readFile("test-file.txt", () => {
+  console.log("I/O finished");
+
+  setTimeout(() => console.log("Timer 2 finished"), 0);
+  setTimeout(() => console.log("Timer 3 finished"), 3000);
+  setImmediate(() => console.log("Imediate 2 finished"));
+
+  process.nextTick(() => {
+    console.log("process.next tick");
